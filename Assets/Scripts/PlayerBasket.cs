fad5b5b54404a9636892234cc5ef438f72eb1503
kongxx3282 <kongxx3282@gmail.com>
2021-10-20 20:58:48 -0400

    VFX

--- Assets/Scripts/PlayerBasket.cs
+++ Assets/Scripts/PlayerBasket.cs
@@ -7,6 +7,9 @@ public class PlayerBasket : MonoBehaviour
     private AudioManager audioManager;
     private UIManager uiManager;
     public int score;
+    public GameObject explosionPrefab;
+    public GameObject confettiPrefab;
+    public GameObject stardustPrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -27,6 +30,18 @@ public class PlayerBasket : MonoBehaviour
             if(fObj.data.type == "Bomb")
             {
                 //end game? or play other sounds etc
+                GameObject explosionEffect = Instantiate(explosionPrefab, fObj.transform.position, fObj.transform.rotation);
+                Destroy(explosionEffect, 3);
+            }
+            else if(fObj.data.type == "Gift")
+            {
+                GameObject confettiEffect = Instantiate(confettiPrefab, fObj.transform.position, fObj.transform.rotation);
+                Destroy(confettiEffect, 3);
+            }
+            else if (fObj.data.type == "Star")
+            {
+                GameObject stardustEffect = Instantiate(stardustPrefab, fObj.transform.position, fObj.transform.rotation);
+                Destroy(stardustEffect, 3);
             }
             audioManager.Play("ItemGet");
             score += fObj.data.points;
